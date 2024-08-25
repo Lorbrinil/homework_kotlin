0b322b2f73501f3ceee776bd687a953f699dcfb3
spopova <spopova@samokat.ru>
2024-08-25 22:39:00 +0300

    Chapter 2 tasks

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,25 @@
 fun main() {
+    homework_1_1()
+    sep()
+    homework_1_2()
+    sep()
+    homework_1_3()
+    sep()
+    homework_1_4()
+    sep()
     homework_1_5()
+    sep()
+    homework_2_1()
+    sep()
+    homework_2_2()
+    sep()
+    homework_2_3()
+    sep()
+    homework_2_4()
+}
+
+fun sep(){
+    println("_________________________________")
 }
 
 fun homework_1_1(){
@@ -52,16 +72,59 @@ fun homework_1_5() {
         print("$x$y ")
         x = x + 1
     }
+    println("")
 }
 
+//в цикле изменить с 5 на 4, потому что массив из 4 элементов
 fun homework_2_1() {
-
+    val hobbits = arrayOf("Frodo", "Sam", "Merry", "Pippin")
+    var x = 0;
+    while (x < 4) {
+        println("${hobbits[x]} is a good Hobbit name")
+        x = x + 1
+    }
 }
 
+// не хватало фигурных скобок в конце вывода
 fun homework_2_2() {
-
+    val firemen = arrayOf("Pugh", "Pugh", "Barney McGrew", "Cuthbert", "Dibble", "Grub")
+    var firemanNo = 0;
+    while (firemanNo < 6) {
+        println("Fireman number $firemanNo is ${firemen[firemanNo]}")
+        firemanNo = firemanNo + 1
+    }
 }
 
 fun homework_2_3() {
+    val fruit = arrayOf("Apple", "Banana", "Cherry", "Blueberry", "Pomegranate")
+    val index = arrayOf(1, 3, 4, 2)
+    var y: Int
+    var x = 0
 
+    while (x < 4) {
+        y = index[x]
+        println("Fruit = ${fruit[y]}")
+        x = x + 1
+    }
+}
+
+fun homework_2_4() {
+    val x = arrayOf(0, 1, 2, 3, 4)
+    x[3] = x[2] //x[3] = 2
+    x[4] = x[0] //x[4] = 0
+    x[2] = x[1] //x[2] = 1
+    x[1] = x[0] //x[1] = 0
+    x[0] = x[1] //x[0] = 0
+    x[4] = x[3] //x[4] = 2
+    x[3] = x[2] //x[3] = 1
+    x[2] = x[4] //x[2] = 2
+    //ИТОГО: 0 0 2 1 2
+
+    //проверяем:
+    var y = 0
+    while (y < 5){
+        println("${x[y]}")
+        y = y + 1
+    }
+//За работу
 }
