e853da82ee1590a89326a877ba5a4809aa13e630
spopova <spopova@samokat.ru>
2024-11-19 21:32:20 +0300

    Chapters 9 - first task

---
+++ src/page291.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+    var a: MutableList<String> = mutableListOf()
+    a.add(0, "Zero")
+    a.add(1, "Two")
+    a.add(2, "Four")
+    a.add(3, "Six")
+    println(a)
+    if (a.contains("Zero")) a.add("Eight")
+    a.removeAt(0)
+    println(a)
+    if (a.contains("Zero")) a.add("Twelve")
+    if (a.indexOf("Four") != 4) a.add("Ten")
+    println(a)
+    println(a)
+}
+
+//ответ из учебника
+/*fun main(args: Array<String>) {
+    var a: MutableList<String> = mutableListOf()
+    a.add(0, "Zero")
+    a.add(1, "Two")
+    a.add(2, "Four")
+    a.add(3, "Six")
+    println(a)
+    if (a.contains("Zero")) a.add("Eight")
+    a.removeAt(0)
+    println(a)
+    if (a.indexOf("Four") != 4) a.add("Ten")
+    println(a)
+    if (a.contains("Zero")) a.add("Twelve")
+    println(a)
+}*/
